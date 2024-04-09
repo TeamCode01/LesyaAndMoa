219701968c20dec1fdb5f2c0adac53ff87e7ade7
Sergio Alferov <alferovsergio@yandex.ru>
2024-04-09 19:52:50 +0300

    хедер готова верстка и начат футер

---
+++ src/shared/components/buttons/button.vue
@@ -0,0 +1,102 @@
+<template>
+    <button
+        :class="[
+            'btn',
+            `btn_${color}`,
+            { btn_rounded: rounded },
+            { btn_outlined: outlined },
+            { btn_large: size === 'large' },
+        ]"
+        :disabled="disabled"
+        @click="clickOnButton"
+    >
+        <span>{{ label }}</span>
+    </button>
+</template>
+<script setup>
+import { ref, computed } from 'vue';
+
+const res = ref();
+
+const props = defineProps({
+    label: {
+        type: String,
+        default: 'Button',
+    },
+    color: {
+        type: String,
+        default: 'primary',
+    },
+    disabled: {
+        type: Boolean,
+        required: false,
+    },
+    rounded: {
+        type: Boolean,
+        required: false,
+    },
+    outlined: {
+        type: Boolean,
+        required: false,
+    },
+    size: {
+        type: String,
+        default: 'normal',
+    },
+});
+
+const emit = defineEmits(['click']);
+
+const clickOnButton = () => {
+    emit('click');
+};
+</script>
+<style lang="scss" scoped>
+.btn {
+    display: block;
+    // height: 52px;
+    box-shadow: none;
+    border-radius: 30px;
+    border: none;
+    cursor: pointer;
+    font-size: 20px;
+    font-weight: 600;
+    font-family: "Nunito", sans-serif;
+    transition: 0.2s;
+    text-align: center;
+    text-transform: none;
+    &_primary {
+        background: $button-primary;
+        color: $layout;
+        height: 51px;
+    }
+    &_second {
+        background: $button-secondary;
+        color: $layout;
+        font-size: 20px;
+        font-weight: 600;
+    }
+    &_info {
+        background: $layout;
+        color: $black;
+        font-size: 20px;
+        font-weight: 600;
+        padding: 8px 24px;
+        height: 43px;
+    }
+    &:disabled {
+        opacity: 0.6;
+        cursor: default;
+    }
+    &_outlined {
+        background: transparent;
+        color: #000;
+        &:hover {
+            color: #fff;
+        }
+    }
+    &_large {
+        padding: 16px 32px;
+    }
+}
+</style>
--- src/widgets/AppFooter/components/Footer.vue
+++ src/widgets/AppFooter/components/Footer.vue
@@ -1,5 +1,66 @@
 <template>
-    <div>Footer</div>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__wrapper">
+                <div class="footer__wrapper_info">
+                    <div class="footer_logo">
+                        <a href="#" target="_blank">
+                            <img
+                                src="@app/assets/icons/LogoLesya.png"
+                                alt="Логотип сайта Леся и Моа"
+                            />
+                        </a>
+                    </div>
+                    <div class="footer__wrapper_text">
+                        <p>© 2024 Организация</p>
+                        <p>Все права защищены</p>
+                    </div>
+                </div>
+                <div class="footer__wrapper_info">
+                    <div class="footer__wrapper_phone">8 (900) 123-45-45</div>
+                    <div class="footer__wrapper_text">
+                        <p>Политика конфиденциональности</p>
+                        <p>Пользовательское соглашение</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
 </template>
 <script setup></script>
-<style lang="scss"></style>
+<style lang="scss">
+.footer {
+    padding: 28px 0px 55px 0px;
+    background-color: $footer;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    &__wrapper {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        a {
+            img {
+                height: 40px;
+                width: 110px;
+                @media (max-width: 1024px) {
+                    height: 30px;
+                    width: 80px;
+                }
+            }
+        }
+        &_text {
+            font-size: 16px;
+            font-weight: 400;
+            font-family: 'Nunito', sans-serif;
+            margin-top: 40px;
+        }
+        &_phone {
+            font-size: 20px;
+            font-weight: 400;
+            font-family: 'Nunito', sans-serif;
+        }
+    }
+}
+</style>
