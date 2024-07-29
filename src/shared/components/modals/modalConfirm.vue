2bf8af656c5f9e411b086172071e3f0af214c1a5
boyoomi <135057713+boyoomi@users.noreply.github.com>
2024-07-29 09:48:49 +0300

    правка label

---
+++ src/shared/components/modals/modalConfirm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="text-center">
+        <v-dialog v-model="dialog" max-width="505">
+            <template v-slot:activator="{ props: activatorProps }">
+                <div
+                    class="text-none font-weight-regular"
+                    prepend-icon="mdi-account"
+                    variant="tonal"
+                    v-bind="activatorProps"
+                >
+                    <span>{{ label }}</span>
+                </div>
+            </template>
+            <slot></slot>
+        </v-dialog>
+    </div>
+</template>
+
+<script setup>
+import { shallowRef } from 'vue';
+
+const dialog = shallowRef(false);
+const props = defineProps({
+    label: {
+        type: String,
+        default: 'Button',
+    },
+});
+</script>
+<style></style>
