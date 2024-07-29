20b4f12492535fa9e4228e3944862ed7fb4d843e
Коля <kolya@Kola--MacBook-Air.local>
2024-07-29 19:32:58 +0300

    Добавил заголовки для запросов

--- src/mixins/gameAction.ts
+++ src/mixins/gameAction.ts
@@ -5,11 +5,21 @@ const gameActions = {
         startGame(child_or_group_id: number, id: number) {
             HTTP.post(`/answers/${child_or_group_id}/`, {
                 task: id,
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Token ' + localStorage.getItem('Token'),
+                }
             })
         },
         endGame(child_or_group_id: number, id: number) {
             HTTP.post(`/answers/${child_or_group_id}/${id}`, {
                 is_correct: true
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Token ' + localStorage.getItem('Token'),
+                }
             })
         }
     }
